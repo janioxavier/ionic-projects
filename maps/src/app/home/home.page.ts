972239fc2011f58472f93fb32a0afbcead490a0c
Jânio Xavier <janiofx@gmail.com>
2019-11-18 20:53:30 -0300

    configuração inicial do projeto do maps

---
+++ maps/src/app/home/home.page.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+
+import { Map, latLng, tileLayer, Layer, marker, Icon } from 'leaflet';
+
+/* Para o ícone do marker carregar nos dispositivos moveis 
+@see https://github.com/Leaflet/Leaflet/issues/4968
+*/
+function habilitarMarkerIcon() {
+  delete Icon.Default.prototype._getIconUrl;
+  
+  Icon.Default.mergeOptions({
+    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+    iconUrl: require('leaflet/dist/images/marker-icon.png'),
+    shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+  });
+}
+
+habilitarMarkerIcon()
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  map: Map;
+
+  ionViewDidEnter() { this.leafletMap(); }
+
+  leafletMap() {
+    // In setView add latLng and zoom
+    this.map = new Map('mapId').setView([28.644800, 77.216721], 10);
+    tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
+      attribution: 'edupala.com © ionic LeafLet',
+    }).addTo(this.map);
+
+    marker([28.6, 77]).addTo(this.map)
+      .bindPopup('Ionic 4 <br> Leaflet.')
+      .openPopup();
+  }
+
+}
